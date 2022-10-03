35493b90f59e3f9c2a03ac01e445fa850dd34acb
Mina Ghaly <minanabil@msn.com>
2022-10-04 08:43:50 +1100

    friends feature added

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,12 +5,18 @@ const { User } = require("../../models");
 router
     .get('/:id', async (req, res) => {
         const id = req.params.id;
-        const user = await User.findOne({ _id: id })
+        const user =
+            await User
+                .findOne({ _id: id })
+                .populate('friends')
+                .populate('thoughts')
         res.send(user);
     })
     .get('/', async (req, res) => {
         const users = await User.find();
-        res.send(users);
+        // res.send(users);
+        res.json(users);
+
     })
     .post('/', async (req, res) => {
         try {
@@ -32,24 +38,32 @@ router
     .delete('/:id', async (req, res) => {
         const user = await User.findOneAndDelete({ _id: req.params.id });
         if (!user) res.status(404).json({ message: 'No user found with this id' })
-        else {User.deleteMany({ _id: { $in: user.thoughts } })
-        res.send(user);
-    }
+        else {
+            User.deleteMany({ _id: { $in: user.thoughts } })
+            res.send(user);
+        }
     }
     )
-    .post('/:userId/friends/:friendId',async(req,res)=>{
-         const userId = req.params.userId;
-         const friendId = req.params.friendId;
-         const user = await User.findOne({ _id: userId });
-         user.updateOne(
-            { _id: userId},
-            { $addToSet: { friends: friendId } }
+    .post('/:userId/friends/:friendId', async (req, res) => {
+        const userId = req.params.userId;
+        const friendId = req.params.friendId;
+        const user = await User.findOneAndUpdate({ userId }, { $addToSet: { friends: friendId } }, { new: true });
+        User.updateOne(user, { $addToSet: { friends: friendId } });
+        res.send(user);
+    }
+    )
+    .delete('/:userId/friends/:friendId', async (req, res) => {
+        const userId = req.params.userId;
+        const friendId = req.params.friendId;
+        const friend = await User.updateOne({ userId }, {
+            $pull: {
+                friends: friendId,
+            },
+        });
 
+        res.send(friend);
 
-         )
+    })
 
-         res.send('user updated');    
-        })
-     
 
 module.exports = router;
